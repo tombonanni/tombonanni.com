2e71ffa5dde4d750e1960c8e7a4357bd52067e38
tombonanni <tombonanni@gmail.com>
2016-11-28 18:42:07 -0500

    added chat example to projects

--- client/js/app.js
+++ client/js/app.js
@@ -1,10 +1,15 @@
 var myApp = angular.module('myApp', [
-  'ngRoute']).
+  'ngRoute',
+  'btford.socket-io']).
+  factory('Socket', function(socketFactory){
+    return socketFactory();
+  }).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
     $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeController'});
     $routeProvider.when('/features', {templateUrl: 'partials/features.html', controller: 'featuresController'});
     $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'projectsController'});
     $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: 'aboutController'});
+    $routeProvider.when('/chat', {templateUrl: 'partials/chat.html', controller: 'chatController'});
     $routeProvider.otherwise({redirectTo: '/home'});
 
     $locationProvider.html5Mode({enabled: true, requireBase: false});
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var app = express();
 
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var path = require('path');
 var port = 3001;
 
-var app = express(); // create our app
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+var mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(methodOverride());
 
 // View Engine setup
 app.set('view engine', 'ejs');
@@ -12,10 +21,49 @@ app.set('views', path.resolve(__dirname, 'client', 'views'));
 
 app.use(express.static(path.resolve(__dirname, 'client')));
 
+var users = [];
+io.on('connection', function(socket){
+	var username = '';
+	console.log("a user has connected");
+
+	socket.on('request-users', function(){
+		socket.emit('users', {users: users});
+	});
+
+	socket.on('message', function(data){
+		io.emit('message', {username: username, message: data.message});
+	});
+
+	socket.on('add-user', function(data){
+		if (users.indexOf(data.username) == -1){
+			io.emit('add-user', {
+				username: data.username
+			});
+			username = data.username;
+			users.push(data.username);
+		} else {
+			socket.emit('prompt-username', {
+				message: 'User Already Exists'
+			});
+		}
+
+	});
+
+	socket.on('disconnect', function(){
+		console.log(username + ' has disconnected');
+		users.splice(users.indexOf(username), 1);
+		io.emit('remove-user', {username: username});
+	});
+});
+
 app.get('/', function(req, res){
 	res.render('index.ejs');
 });
 
-app.listen(port, function(){
+app.get('/*', function(req, res){
+	res.render('index.ejs');
+});
+
+http.listen(port, function(){
   console.log(new Date().toISOString() + ': server started on ' + port);
 });
